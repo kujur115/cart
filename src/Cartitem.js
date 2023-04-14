5ab4f498f19c44414495e825edbcac0316f41a93
Pratik Kujur <kujurpratik115@gmail.com>
2023-04-14 12:05:15 +0530

    setState to increase and decrease the quantity

--- src/Cartitem.js
+++ src/Cartitem.js
@@ -11,7 +11,27 @@ class Cartitem extends React.Component {
     };
   }
   increaseQuantity = () => {
-    this.setState({ qty: this.props.qty + 1 });
+    // ? type 1
+    // this.setState({ qty: this.state.qty + 1 });
+    // ? type 2 - if previous State is required
+    this.setState((prevState) => {
+      return {
+        qty: prevState.qty + 1,
+      };
+    });
+  };
+  decreaseQuantity = () => {
+    this.setState((prevState) => {
+      if (prevState.qty > 0) {
+        return {
+          qty: prevState.qty - 1,
+        };
+      } else {
+        return {
+          qty: 0,
+        };
+      }
+    });
   };
   render() {
     const { price, title, qty } = this.state;
@@ -29,19 +49,19 @@ class Cartitem extends React.Component {
             <img
               alt="increase"
               className="action-icons"
-              src="https://image.flaticon.com/icons/svg/992/992651.svg"
+              // src="https://image.flaticon.com/icons/svg/992/992651.svg"
+              src="./plus.png"
               onClick={this.increaseQuantity}
             />
+            {/* <a href="https://www.flaticon.com/free-icons/plus" title="plus icons">Plus icons created by dmitri13 - Flaticon</a> */}
+            {/* <i class="fa-light fa-circle-plus"></i> */}
             <img
               alt="decrease"
               className="action-icons"
-              src="https://image.flaticon.com/icons/svg/1665/1665612.svg"
+              src="./minus-sign.png"
+              onClick={this.decreaseQuantity}
             />
-            <img
-              alt="delete"
-              className="action-icons"
-              src="https://image.flaticon.com/icons/svg/1214/1214428.svg"
-            />
+            <img alt="delete" className="action-icons" src="./delete.png" />
           </div>
         </div>
       </div>
