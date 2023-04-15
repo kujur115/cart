2bf7f25f91aee8b0bf9a602dafd816d33a2c3e59
Pratik Kujur <kujurpratik115@gmail.com>
2023-04-15 12:36:52 +0530

    Delete product from Cart

--- src/Cart.js
+++ src/Cart.js
@@ -37,12 +37,52 @@ class Cart extends React.Component {
       ],
     };
   }
+
+  handleIncreaseQuantity = (product) => {
+    console.log("Hey,please inc the qty of", product);
+    const { products } = this.state;
+    const index = products.indexOf(product);
+
+    products[index].qty += 1;
+    this.setState({
+      products: products,
+    });
+  };
+
+  handleDecreaseQuantity = (product) => {
+    console.log("Hey,please inc the qty of", product);
+    const { products } = this.state;
+    const index = products.indexOf(product);
+    if (products[index].qty === 0) return;
+    products[index].qty -= 1;
+    this.setState({
+      products: products,
+    });
+  };
+
+  handleDeleteProduct = (id) => {
+    const { products } = this.state;
+    const items = products.filter((item) => item.id !== id);
+
+    this.setState({
+      products: items,
+    });
+  };
+
   render() {
     const { products } = this.state;
     return (
       <div className="cart">
         {products.map((product) => {
-          return <Cartitem product={product} key={product.id} />;
+          return (
+            <Cartitem
+              product={product}
+              key={product.id}
+              onIncreaseQuantity={this.handleIncreaseQuantity}
+              onDecreaseQuantity={this.handleDecreaseQuantity}
+              handleDeleteProduct={this.handleDeleteProduct}
+            />
+          );
         })}
       </div>
     );
--- src/Cartitem.js
+++ src/Cartitem.js
@@ -1,46 +1,37 @@
 import React from "react";
 
 class Cartitem extends React.Component {
-  // testing() {
-  //   const promise = new Promise((resolve, reject) => {
-  //     setTimeout(() => {
-  //       resolve("done");
-  //     }, 5000);
+  // increaseQuantity = () => {
+  //   // ? type 1
+  //   // this.setState({ qty: this.state.qty + 1 });
+  //   // ? type 2 - if previous State is required
+  //   this.setState((prevState) => {
+  //     return {
+  //       qty: prevState.qty + 1,
+  //     };
   //   });
-  //   promise.then(() => {
-  //      ? setState acts as synchronus call
-  //     this.setState({ qty: this.state.qty + 10 });
-  //     this.setState({ qty: this.state.qty + 10 });
-  //     this.setState({ qty: this.state.qty + 10 });
-
-  //     console.log("state", this.state);
+  // };
+  // decreaseQuantity = () => {
+  //   this.setState((prevState) => {
+  //     if (prevState.qty > 0) {
+  //       return {
+  //         qty: prevState.qty - 1,
+  //       };
+  //     } else {
+  //       return {
+  //         qty: 0,
+  //       };
+  //     }
   //   });
-  // }
-  increaseQuantity = () => {
-    // ? type 1
-    // this.setState({ qty: this.state.qty + 1 });
-    // ? type 2 - if previous State is required
-    this.setState((prevState) => {
-      return {
-        qty: prevState.qty + 1,
-      };
-    });
-  };
-  decreaseQuantity = () => {
-    this.setState((prevState) => {
-      if (prevState.qty > 0) {
-        return {
-          qty: prevState.qty - 1,
-        };
-      } else {
-        return {
-          qty: 0,
-        };
-      }
-    });
-  };
+  // };
   render() {
     const { price, title, qty } = this.props.product;
+    const {
+      product,
+      onIncreaseQuantity,
+      onDecreaseQuantity,
+      handleDeleteProduct,
+    } = this.props;
     return (
       <div className="cart-item">
         <div className="left-block">
@@ -55,19 +46,21 @@ class Cartitem extends React.Component {
             <img
               alt="increase"
               className="action-icons"
-              // src="https://image.flaticon.com/icons/svg/992/992651.svg"
               src="./plus.png"
-              onClick={this.increaseQuantity}
+              onClick={() => onIncreaseQuantity(product)}
             />
-            {/* <a href="https://www.flaticon.com/free-icons/plus" title="plus icons">Plus icons created by dmitri13 - Flaticon</a> */}
-            {/* <i class="fa-light fa-circle-plus"></i> */}
             <img
               alt="decrease"
               className="action-icons"
               src="./minus-sign.png"
-              onClick={this.decreaseQuantity}
+              onClick={() => onDecreaseQuantity(product)}
             />
-            <img alt="delete" className="action-icons" src="./delete.png" />
+            <img
+              alt="delete"
+              className="action-icons"
+              src="./delete.png"
+              onClick={() => handleDeleteProduct(product.id)}
+            />
           </div>
         </div>
       </div>
